0d43516dde0193d574e840e1bb44fade3ea4c80c
Mani Gandham <manigandham@gmail.com>
2021-04-27 17:07:44 -0700

    test fs logging

---
+++ src/ConsoleApp1/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open Serilog
+open Serilog.Events
+open Serilog.Sinks.FastConsole
+
+let logger =
+         LoggerConfiguration()
+             .MinimumLevel.Debug()
+             .WriteTo.FastConsole()
+             .WriteTo.File("test.log", shared = true, restrictedToMinimumLevel = LogEventLevel.Debug)
+             .CreateLogger()
+
+[<EntryPoint>]
+let main argv =
+    logger.Debug("test logging message")
+    0 // return an integer exit code
