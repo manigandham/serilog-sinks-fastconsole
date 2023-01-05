f96d6d56bc731de9fb70be523743ac445a421dad
Mani Gandham <manigandham@gmail.com>
2023-01-05 01:17:01 -0800

    improve channel handling
    - use StreamWriter with higher internal buffer (used for file I/O)
    - disable autoflush
    - set blocking options directly in channel config
    - early bypass if no blocking required
    - use null instead of empty string when writing nothing to StringWriter
    - improve dispose without finalizers

--- src/Serilog.Sinks.FastConsole/FastConsoleSink.cs
+++ src/Serilog.Sinks.FastConsole/FastConsoleSink.cs
@@ -12,44 +12,43 @@ namespace Serilog.Sinks.FastConsole;
 
 public class FastConsoleSink : ILogEventSink, IDisposable
 {
-    private readonly StreamWriter _consoleWriter = new(Console.OpenStandardOutput(), Console.OutputEncoding) { AutoFlush = true };
+    private static readonly StreamWriter _consoleWriter = new(Console.OpenStandardOutput(), Console.OutputEncoding, 4096, true);
     private readonly StringWriter _bufferWriter = new();
     private readonly Channel<LogEvent?> _queue;
     private readonly Task _worker;
-
     private readonly FastConsoleSinkOptions _options;
     private readonly ITextFormatter? _textFormatter;
     private readonly JsonValueFormatter _valueFormatter = new();
-    private readonly bool _bounded;
+    private readonly bool _waitForQueue;
 
     public FastConsoleSink(FastConsoleSinkOptions options, ITextFormatter? textFormatter)
     {
         _options = options;
         _textFormatter = textFormatter;
-        _bounded = options.QueueLimit > 0;
 
-        _queue = _bounded
-            ? Channel.CreateBounded<LogEvent?>(new BoundedChannelOptions(options.QueueLimit!.Value) { SingleReader = true })
+        _waitForQueue = options.QueueLimit > 0 && options.BlockWhenFull == true;
+        _queue = options.QueueLimit > 0
+            ? Channel.CreateBounded<LogEvent?>(new BoundedChannelOptions(options.QueueLimit!.Value) { SingleReader = true, FullMode = _options.BlockWhenFull ? BoundedChannelFullMode.Wait : BoundedChannelFullMode.DropWrite })
             : Channel.CreateUnbounded<LogEvent?>(new UnboundedChannelOptions { SingleReader = true });
 
         _worker = Task.Factory.StartNew(WriteToConsoleStream, CancellationToken.None, TaskCreationOptions.LongRunning | TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
     }
 
-    // logs are immediately queued to channel
     public void Emit(LogEvent logEvent)
     {
-        if (!_queue.Writer.TryWrite(logEvent) && _bounded && _options.BlockWhenFull)
-            Task.Run(() => _queue.Writer.WriteAsync(logEvent)).ConfigureAwait(false).GetAwaiter().GetResult();
+        // logs are immediately written to the underlying System.Threading.Channel which handles queue size and blocking automatically
+        // `_waitForQueue` check is used to save task overhead when not blocking for writes
+        if (!_queue.Writer.TryWrite(logEvent) && _waitForQueue)
+            _queue.Writer.WriteAsync(logEvent).AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
     }
 
     private async Task WriteToConsoleStream()
     {
-        // cache reference to stringbuilder inside writer
-        // console output is an IO stream
-        // format and write event to in-memory buffer and then flush to console async   
-        // do not use the locking textwriter from console.out used by console.writeline
-
+        // cache reference to StringBuilder inside StringWriter
         var sb = _bufferWriter.GetStringBuilder();
+
+        // console output is an IO stream, so logs are formatted to an in-memory buffer and then written to console asynchronously  
+        // do not use the locking textwriter from console.out used by console.writeline
 
 #if NET5_0_OR_GREATER
         await foreach (var logEvent in _queue.Reader.ReadAllAsync().ConfigureAwait(false))
@@ -72,7 +71,7 @@ public class FastConsoleSink : ILogEventSink, IDisposable
             await _consoleWriter.WriteAsync(sb.ToString()).ConfigureAwait(false);
 #endif
 
-            // must clear buffer manually
+            // must clear StringBuilder buffer manually
             sb.Clear();
         }
 
@@ -109,7 +108,7 @@ public class FastConsoleSink : ILogEventSink, IDisposable
         {
             writer.Write(",\"properties\":{");
 
-            var precedingDelimiter = "";
+            string? precedingDelimiter = null;
             foreach (var kvp in e.Properties)
             {
                 writer.Write(precedingDelimiter);
@@ -143,28 +142,30 @@ public class FastConsoleSink : ILogEventSink, IDisposable
 
     private bool _disposed; // to detect redundant calls
 
-    public void Dispose() => Dispose(true);
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 
     protected virtual void Dispose(bool disposing)
     {
-        if (_disposed)
-            return;
-
-        if (disposing)
+        if (!_disposed)
         {
-            // Close write queue and wait until items are drained
-            // then wait for all console output to be flushed.
-            // Use TryComplete instead of Complete because Complete throws
-            // System.Threading.Channels.ChannelClosedException: 'The channel has been closed.'
-            // in case of concurrent Dispose calls.
-            _queue.Writer.TryComplete(null);
-            _worker.GetAwaiter().GetResult();
+            if (disposing)
+            {
+                // Close write queue and wait until items are drained
+                // then wait for all console output to be flushed.
+                // Use TryComplete instead of Complete because Complete throws
+                // System.Threading.Channels.ChannelClosedException: 'The channel has been closed.'
+                // in case of concurrent Dispose calls.
+                _queue.Writer.TryComplete();
+                _worker.ConfigureAwait(false).GetAwaiter().GetResult();
+                _consoleWriter.Dispose();
+            }
 
-            _bufferWriter.Dispose();
-            _consoleWriter.Dispose();
+            _disposed = true;
         }
-
-        _disposed = true;
     }
 
     #endregion
