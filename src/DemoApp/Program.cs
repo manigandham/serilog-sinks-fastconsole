0389e2ac24271a8cb03bdaa631e88214c2b9fe1d
Mani Gandham <manigandham@gmail.com>
2023-01-05 01:17:48 -0800

    updated test app with simpler logging setup and results

--- src/DemoApp/Program.cs
+++ src/DemoApp/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.FastConsole;
@@ -13,52 +15,48 @@ Random _random = new();
 // enable serilog to log out internal messages to console for debugging
 Serilog.Debugging.SelfLog.Enable(Console.WriteLine);
 
-// see serilog .net 6 integration: https://blog.datalust.co/using-serilog-in-net-6/
-Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
-Log.Information("Starting up");
+// // see serilog .net 6 integration: https://blog.datalust.co/using-serilog-in-net-6/
+// Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+// Log.Information("Starting up");
 
-try
+var threadsCount = 50;
+var logsCount = 100;
+var sinkOptions = new FastConsoleSinkOptions { UseJson = true, QueueLimit = 1000, BlockWhenFull = true };
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((ctx, lc) =>
 {
-    var logsCount = 5000;
-    var sinkOptions = new FastConsoleSinkOptions { /*QueueLimit = 10, BlockWhenFull = true*/ };
+    lc.MinimumLevel.Debug().Enrich.FromLogContext().WriteTo.FastConsole(sinkOptions);
+    //.WriteTo.File("log.txt", shared: true);
+    //.WriteTo.Console()
+});
 
-    var builder = WebApplication.CreateBuilder(args);
-    builder.Host.UseSerilog((ctx, lc) =>
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+app.MapGet("/", async ([FromServices] Microsoft.Extensions.Logging.ILogger<Program> _logger) =>
+{
+    var sw = Stopwatch.StartNew();
+    var tasks = Enumerable.Range(1, threadsCount).Select(t => Task.Run(() =>
     {
-        lc.MinimumLevel.Debug().Enrich.FromLogContext().WriteTo.FastConsole(sinkOptions);
-        //.WriteTo.File("log.txt", shared: true, restrictedToMinimumLevel: LogEventLevel.Debug)
-        //.WriteTo.Console()
-    });
+        for (int i = 0; i < logsCount; i++)
+            _logger.LogInformation("{thread} {log} {msg}", t, i, CreateRandomString(100));
+    }));
 
-    var app = builder.Build();
-    app.UseSerilogRequestLogging();
+    await Task.WhenAll(tasks);
 
-    app.MapGet("/", async ([FromServices] Microsoft.Extensions.Logging.ILogger<Program> _logger) =>
-    {
-        var sw = Stopwatch.StartNew();
-        var tasks = Enumerable.Range(1, logsCount).Select(t => Task.Run(() => _logger.LogInformation(CreateRandomString(100))));
-        await Task.WhenAll(tasks);
+    var msg =
+    $"Request ID: {Guid.NewGuid()}" +
+    $"\n logs: {logsCount}" +
+    $"\n queue limit: {sinkOptions.QueueLimit}" +
+    $"\n block when full: {sinkOptions.BlockWhenFull}" +
+    $"\n milliseconds: {sw.ElapsedMilliseconds}" +
+    $"\n threads: {Process.GetCurrentProcess().Threads.Count}";
 
-        return $"Logging random messages to console" +
-        $"\n logs: {logsCount}" +
-        $"\n queue limit: {sinkOptions.QueueLimit}" +
-        $"\n queue block: {sinkOptions.BlockWhenFull}" +
-        $"\n milliseconds: {sw.ElapsedMilliseconds}" +
-        $"\n threads: {Process.GetCurrentProcess().Threads.Count}";
-    });
+    return msg;
+});
 
-    app.Run();
-
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Unhandled exception");
-}
-finally
-{
-    Log.Information("Shut down complete");
-    Log.CloseAndFlush();
-}
+await app.RunAsync();
+Log.CloseAndFlush();
 
 
 string CreateRandomString(int length)
